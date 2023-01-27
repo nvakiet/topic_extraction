c17eae5eab1123da1c8ef45d4a138ce3371fb924
nvakiet <nvakiet191@gmail.com>
2023-01-28 00:43:12 +0700

    Initialize project structure with Cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='topic_extractor',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A machine learning application to extract top N topics from a corpus of texts and images.',
+    author='nvakiet',
+    license='MIT',
+)
